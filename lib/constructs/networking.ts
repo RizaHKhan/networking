7383ce2a70ecf04855a8c3edacc4f1dab58ab46d
RizaHKhan <khanriza@gmail.com>
2025-04-27 15:41:26 -0400

    updating subnet configuration

--- lib/constructs/networking.ts
+++ lib/constructs/networking.ts
@@ -3,6 +3,7 @@ import {
     IpAddresses,
     IpProtocol,
     Ipv6Addresses,
+    SubnetType,
     Vpc,
 } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
@@ -18,10 +19,34 @@ interface Exports {
 export default ({ scope }: Props): Exports => {
     const vpc = new Vpc(scope, "VPC", {
         vpcName: "a4l-vpc1",
-        ipAddresses: IpAddresses.cidr("10.16.0.0/16"),
+        maxAzs: 4, // Default is all AZs in region
+        ipAddresses: IpAddresses.cidr("10.16.0.0/16"), // 10.16.0.0 -> 10.16.255.255
         defaultInstanceTenancy: DefaultInstanceTenancy.DEFAULT,
         ipProtocol: IpProtocol.DUAL_STACK,
         ipv6Addresses: Ipv6Addresses.amazonProvided(),
+        subnetConfiguration: [
+            {
+                name: "DB",
+                subnetType: SubnetType.PRIVATE_ISOLATED,
+                cidrMask: 20,
+            },
+            {
+                name: "App",
+                subnetType: SubnetType.PRIVATE_ISOLATED,
+                cidrMask: 20,
+            },
+            {
+                name: "Web",
+                subnetType: SubnetType.PRIVATE_ISOLATED,
+                cidrMask: 20,
+            },
+            {
+                name: "Reserved",
+                subnetType: SubnetType.PRIVATE_ISOLATED,
+                cidrMask: 20,
+                reserved: true,
+            },
+        ],
     });
 
     return { vpc };
