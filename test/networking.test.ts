82d50165b38c358263afc47fc50f6d0fe05b0c1f
RizaHKhan <khanriza@gmail.com>
2025-05-23 20:49:33 -0400

    splitting out tests

--- test/networking.test.ts
+++ test/networking.test.ts
@@ -1,25 +1,36 @@
 import { App } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-import * as Networking from "../lib/networking-stack";
 import { SubnetType } from "aws-cdk-lib/aws-ec2";
+import * as Networking from "../lib/networking-stack";
 
-test("VPC Created", () => {
-    const app = new App();
-    const stack = new Networking.NetworkingStack(app, "MyTestStack");
-    const template = Template.fromStack(stack);
+describe("Networking Stack", () => {
+    test("VPC Created", () => {
+        const app = new App();
+        const stack = new Networking.NetworkingStack(app, "MyTestStack");
+        const template = Template.fromStack(stack);
 
-    // Has a VPC
-    const vpc = template.hasResourceProperties("AWS::EC2::VPC", {
-        CidrBlock: "10.0.0.0/16",
+        // Has a VPC
+        template.hasResourceProperties("AWS::EC2::VPC", {
+            CidrBlock: "10.0.0.0/16",
+        });
+
+        template.resourceCountIs("AWS::EC2::Subnet", 2);
+
+        // Has a private subnet
+        template.hasResourceProperties("AWS::EC2::Subnet", {
+            CidrBlock: "10.0.0.0/24",
+        });
     });
 
-    // Has a private subnet
-    template.hasResourceProperties("AWS::EC2::Subnet", {
-        CidrBlock: "10.0.0.0/24",
-    });
+    test("One EC2 created", () => {
+        const app = new App();
+        const stack = new Networking.NetworkingStack(app, "MyTestStack");
+        const template = Template.fromStack(stack);
 
-    // EC2 instance is in the private subnet
-    template.hasResourceProperties("AWS::EC2::Instance", {
-        InstanceType: "t3.micro",
+        // EC2 instance is in the private subnet
+        template.resourceCountIs("AWS::EC2::Instance", 1);
+        template.hasResourceProperties("AWS::EC2::Instance", {
+            InstanceType: "t3.micro",
+        });
     });
 });
